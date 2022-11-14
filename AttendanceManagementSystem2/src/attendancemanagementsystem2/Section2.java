61db1fd405a336230ae69483b64cf8c87558cd81
Dukz17 <joseduke325@gmail.com>
2022-11-14 10:01:52 +0800

    Update Section2.java
    Hiding information

--- AttendanceManagementSystem2/src/attendancemanagementsystem2/Section2.java
+++ AttendanceManagementSystem2/src/attendancemanagementsystem2/Section2.java
@@ -1,33 +1,31 @@
 package attendancemanagementsystem2;
-
 public class Section2 extends AttendanceManagementSystem2 {    
-    static void mainfunc2(){
-        Dets Section2_1 = new Dets (201 , 19 , "Hazel-Mae" , "Blanquera" , "Albino" , "Female" , "BSIT2", "N/A");
-        Dets Section2_2 = new Dets (202 , 19 , "Jinel-Hanz" , "Arimas" , "Anacite" , "Male" , "BSIT2", "N/A");
-        Dets Section2_3 = new Dets (203 , 20 , "Selvester" , "Baldon" , "Apor" , "Male" , "BSIT2", "N/A");
-        Dets Section2_4 = new Dets (204 , 20 , "Venz-Jan" , "Achas" , "Cabonegro" , "Male" , "BSIT2" , "N/A");
-        Dets Section2_5 = new Dets (205 , 20 , "Beverly" , "Aba" , "Canoy" , "Male" , "BSIT2", "N/A");
-        Dets Section2_6 = new Dets (206 , 20 , "Angelo" , "Carillo" , "Castro" , "Male" , "BSIT2", "N/A");
-        Dets Section2_7 = new Dets (207 , 20 , "Ernest-Dale" , "Siglos" , "Carton" , "Male" , "BSIT2", "N/A");
-        Dets Section2_8 = new Dets (208 , 20 , "Althea-Dela" , "Vega" , "Dewites" , "Male" , "BSIT2", "N/A");
-        Dets Section2_9 = new Dets (209 , 20 , "Karl-John" , "Regunda" , "Dutosme" , "Male" , "BSIT2", "N/A");
-        Dets Section2_10 = new Dets (210 , 20 , "Kharlo" , "Castro" , "Edulan" , "Male" , "BSIT2", "N/A");
-        Dets Section2_11 = new Dets (211 , 20 , "Joejette" , "Olano" , " Ferrariz" , "Male" , "BSIT2" , "N/A");
-        Dets Section2_12 = new Dets (212 , 20 , "Shan-Ii" , "Maghopoy" , "Gorra" , "Male" , "BSIT2", "N/A");
-        Dets Section2_13 = new Dets (213 , 20 , "Joshua" , "Daguno" , "Gumahad" , "Male" , "BSIT2" ,"N/A");
-        Dets Section2_14 = new Dets (214 , 21 , "Dexter" , "Palarca" , "Maghanoy" , "Male" , "BSIT2", "N/A");
-        Dets Section2_15 = new Dets (215 , 21 , "Brecht" , "Pagtalunan" , "Marquita" , "Male" , "BSIT2", "N/A");
-        Dets Section2_16 = new Dets (216 , 21 , "Andrie-Mark" , "Tudtod" , "Matildo" , "Male" , "BSIT2", "N/A");
-        Dets Section2_17 = new Dets (217 , 20 , "Joemari" , "Uba" , "Obrial" , "Male" , "BSIT2" , "N/A");
-        Dets Section2_18 = new Dets (218 , 20 , "Charls-John" , "Vargas" , "Opiso" , "Male" , "BSIT2", "N/A");
-        Dets Section2_19 = new Dets (219 , 21 , "Feliciano-Sherdy" , "Egoc" , "Pagaran" , "Male" , "BSIT2" , "N/A");
-        Dets Section2_20 = new Dets (220 , 20 , "Rosemari" , "Kilat" , "Roque" , "Female" , "BSIT2" , "N/A");
-        Dets Section2_21 = new Dets (221 , 20 , "Nimriel" , "Longhas" , "Sigua" , "Male" , "BSIT2", "N/A");
-        Dets Section2_22 = new Dets (222 , 20 , "Kizziah-Zandra" , "Francisco" , "Tajor" , "Female" , "BSIT2", "N/A");
-        Dets Section2_23 = new Dets (223 , 20 , "Andrew-Albert" , "Ganaden" , "Talaboc" , "Male" , "BSIT2" , "N/A");
-        Dets Section2_24 = new Dets (224 , 20 , "Herod-Jan" , "Barrientos" , "Tan" , "Male" , "BSIT2", "N/A");
-        Dets Section2_25 = new Dets (225 , 20 , "Rhon-Kenneth" , "N/A" , "Trazo" , "Male" , "BSIT2" , "N/A");
-        
+    public static void mainfunc2(){
+        Dets Section2_1 = new Dets (201 , 19 , "Hazel-Mae" , "" , "" , "Female" , "BSIT2", "N/A");
+        Dets Section2_2 = new Dets (202 , 19 , "Jinel-Hanz" , "" , "" , "Male" , "BSIT2", "N/A");
+        Dets Section2_3 = new Dets (203 , 20 , "Selvester" , "" , "" , "Male" , "BSIT2", "N/A");
+        Dets Section2_4 = new Dets (204 , 20 , "Venz-Jan" , "" , "" , "Male" , "BSIT2" , "N/A");
+        Dets Section2_5 = new Dets (205 , 20 , "Beverly" , "" , "" , "Male" , "BSIT2", "N/A");
+        Dets Section2_6 = new Dets (206 , 20 , "Angelo" , "" , "" , "Male" , "BSIT2", "N/A");
+        Dets Section2_7 = new Dets (207 , 20 , "Ernest-Dale" , "" , "" , "Male" , "BSIT2", "N/A");
+        Dets Section2_8 = new Dets (208 , 20 , "Althea-Dela" , "" , "" , "Male" , "BSIT2", "N/A");
+        Dets Section2_9 = new Dets (209 , 20 , "Karl-John" , "" , "" , "Male" , "BSIT2", "N/A");
+        Dets Section2_10 = new Dets (210 , 20 , "Kharlo" , "" , "" , "Male" , "BSIT2", "N/A");
+        Dets Section2_11 = new Dets (211 , 20 , "Joejette" , "" , "" , "Male" , "BSIT2" , "N/A");
+        Dets Section2_12 = new Dets (212 , 20 , "Shan-Ii" , "" , "" , "Male" , "BSIT2", "N/A");
+        Dets Section2_13 = new Dets (213 , 20 , "Joshua" , "" , "" , "Male" , "BSIT2" ,"N/A");
+        Dets Section2_14 = new Dets (214 , 21 , "Dexter" , "" , "" , "Male" , "BSIT2", "N/A");
+        Dets Section2_15 = new Dets (215 , 21 , "Brecht" , "" , "" , "Male" , "BSIT2", "N/A");
+        Dets Section2_16 = new Dets (216 , 21 , "Andrie-Mark" , "" , "" , "Male" , "BSIT2", "N/A");
+        Dets Section2_17 = new Dets (217 , 20 , "Joemari" , "" , "" , "Male" , "BSIT2" , "N/A");
+        Dets Section2_18 = new Dets (218 , 20 , "Charls-John" , "" , "" , "Male" , "BSIT2", "N/A");
+        Dets Section2_19 = new Dets (219 , 21 , "Feliciano-Sherdy" , "" , "" , "Male" , "BSIT2" , "N/A");
+        Dets Section2_20 = new Dets (220 , 20 , "Rosemari" , "" , "" , "Female" , "BSIT2" , "N/A");
+        Dets Section2_21 = new Dets (221 , 20 , "Nimriel" , "" , "" , "Male" , "BSIT2", "N/A");
+        Dets Section2_22 = new Dets (222 , 20 , "Kizziah-Zandra" , "" , "" , "Female" , "BSIT2", "N/A");
+        Dets Section2_23 = new Dets (223 , 20 , "Andrew-Albert" , "" , "" , "Male" , "BSIT2" , "N/A");
+        Dets Section2_24 = new Dets (224 , 20 , "Herod-Jan" , "" , "" , "Male" , "BSIT2", "N/A");
+        Dets Section2_25 = new Dets (225 , 20 , "Rhon-Kenneth" , "" , "" , "Male" , "BSIT2" , "N/A");
         UniqueID uid1 = new UniqueID(201);
         UniqueID uid2 = new UniqueID(202);
         UniqueID uid3 = new UniqueID(203);
@@ -53,7 +51,6 @@ public class Section2 extends AttendanceManagementSystem2 {
         UniqueID uid23 = new UniqueID(223);
         UniqueID uid24 = new UniqueID(224);
         UniqueID uid25 = new UniqueID(225);
-        
 	studList.add(Section2_1);
         uniqueid.add(uid1);
 	studList.add(Section2_2);
