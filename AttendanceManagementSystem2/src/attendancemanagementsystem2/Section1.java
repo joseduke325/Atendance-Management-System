4f3ffdb2504c23e41be248a77eb8fa87ed621abb
Dukz17 <joseduke325@gmail.com>
2022-11-14 09:54:52 +0800

    Update Section1.java
    Adjusting blank space & delete middle and lastname

--- AttendanceManagementSystem2/src/attendancemanagementsystem2/Section1.java
+++ AttendanceManagementSystem2/src/attendancemanagementsystem2/Section1.java
@@ -1,34 +1,31 @@
 package attendancemanagementsystem2;
-
 public class Section1 extends AttendanceManagementSystem2 {
     public static void main(){
-        
-        Dets Section1_1 = new Dets (101 , 19 , "Marvin" , "Ramos" , "Abella" , "Male" , "BSIT1", "N/A");
-        Dets Section1_2 = new Dets (102 , 20 , "Key-Mhare" , "Diaz" , "Adeva" , "Female" , "BSIT1", "N/A");
-        Dets Section1_3 = new Dets (103 , 20 , "Mirah-Mea" , "Salagan" , "Aguilar" , "Female" , "BSIT1", "N/A");
-        Dets Section1_4 = new Dets (104 , 22 , "Hannah" , "Soaib" , "Angni" , "Female" , "BSIT1", "N/A");
-        Dets Section1_5 = new Dets (105 , 20 , "Duke-Jose" , "Marvilla" , "Aporillo" , "Male" , "BSIT1" , "N/A");
-        Dets Section1_6 = new Dets (106 , 20 , "Joshua" , "Aragon" , "Calma" , "Male" , "BSIT1", "N/A");
-        Dets Section1_7 = new Dets (107 , 21 , "Earl-Justin" , "Taroy" , "Caseres" , "Male" , "BSIT1", "N/A");
-        Dets Section1_8 = new Dets (108 , 21 , "Arnelle" , "Ligutom" , "Dazo" , "Male" , "BSIT1", "N/A");
-        Dets Section1_9 = new Dets (109 , 20 , "Joel" , "Billones" , "Egama" , "Male" , " BSIT1", "N/A");
-        Dets Section1_10 = new Dets (110 , 20 , "Ruben" , "Tagupa" , "Jumawid" , "Male" , "BSIT1", "N/A");
-        Dets Section1_11 = new Dets (111 , 20 , "Fitz-Gerard" , "Daleon" , "Labe" , "Male" , "BSIT1", "N/A");
-        Dets Section1_12 = new Dets (112 , 20 , "Jay-Anne" , "Gua-an" , "Lalanan" , "Female" , "BSIT1", "N/A");
-        Dets Section1_13 = new Dets (113 , 23 , "Nick-Carter" , "Calonge" , "Ligutom" , "Male" , "BSIT1", "N/A");
-        Dets Section1_14 = new Dets (114 , 21 , "James-Ryan" , "Batutay" , "Magsalay" , "Male" , "BSIT1", "N/A");
-        Dets Section1_15 = new Dets (115 , 21 , "Shadrin-Jerome" , "Perame" , "Mopal" , "Male" , "BSIT1", "N/A");
-        Dets Section1_16 = new Dets (116 , 19 , "Karel" , " Palasan" , "Naguita" , "Female" , "BSIT1", "N/A");
-        Dets Section1_17 = new Dets (117 , 19 , "Anadyl" , "Malaque" , "Pagute" , "Female" , "BSIT1", "N/A");
-        Dets Section1_18 = new Dets (118 , 21 , "Hamza" , "Bainarie" , "Pahaman" , "Male" , "BSIT1" , "N/A");
-        Dets Section1_19 = new Dets (119 , 20 , "Kert-Ice" , "Griffing" , "Roque" , "Male" , "BSIT1", "N/A");
-        Dets Section1_20 = new Dets (120 , 21 , "Jeric" , "N/A" , "Salonoy" , "Male" , "BSIT1", "N/A");
-        Dets Section1_21 = new Dets (121 , 20 , "Angelo" , "Corro" , "Serafino" , "Male" , "BSIT1" , "N/A");
-        Dets Section1_22 = new Dets (122 , 19 , "May-Joy" , "Timbal" , "Solis" , "Female" , "BSIT1" , "N/A");
-        Dets Section1_23 = new Dets (123 , 19 , "Mary-Jane" , "Veradio" , "Sulilawan" , "Female" , "BSIT1", "N/A");
-        Dets Section1_24 = new Dets (124 , 21 , "Jazerll" , "Mandantes" , "Velez" , "Male" , "BSIT1", "N/A");
-        Dets Section1_25 = new Dets (125 , 20 , "John-Andrej" , "Picut" , "Zulita" , "Male" , "BSIT1", "N/A");
-        
+        Dets Section1_1 = new Dets (101 , 19 , "Marvin" , "" , "" , "Male" , "BSIT1", "N/A");
+        Dets Section1_2 = new Dets (102 , 20 , "Key-Mhare" , "" , "" , "Female" , "BSIT1", "N/A");
+        Dets Section1_3 = new Dets (103 , 20 , "Mirah-Mea" , "" , "" , "Female" , "BSIT1", "N/A");
+        Dets Section1_4 = new Dets (104 , 22 , "Hannah" , "" , "" , "Female" , "BSIT1", "N/A");
+        Dets Section1_5 = new Dets (105 , 20 , "Duk" , "" , "" , "Male" , "BSIT1" , "N/A");
+        Dets Section1_6 = new Dets (106 , 20 , "Joshua" , "" , "" , "Male" , "BSIT1", "N/A");
+        Dets Section1_7 = new Dets (107 , 21 , "Earl-Justin" , "" , "" , "Male" , "BSIT1", "N/A");
+        Dets Section1_8 = new Dets (108 , 21 , "Arnelle" , "" , "" , "Male" , "BSIT1", "N/A");
+        Dets Section1_9 = new Dets (109 , 20 , "Joel" , "" , "" , "Male" , " BSIT1", "N/A");
+        Dets Section1_10 = new Dets (110 , 20 , "Ruben" , "" , "" , "Male" , "BSIT1", "N/A");
+        Dets Section1_11 = new Dets (111 , 20 , "Fitz-Gerard" , "" , "" , "Male" , "BSIT1", "N/A");
+        Dets Section1_12 = new Dets (112 , 20 , "Jay-Anne" , "" , "" , "Female" , "BSIT1", "N/A");
+        Dets Section1_13 = new Dets (113 , 23 , "Nick-Carter" , "" , "" , "Male" , "BSIT1", "N/A");
+        Dets Section1_14 = new Dets (114 , 21 , "James-Ryan" , "" , "" , "Male" , "BSIT1", "N/A");
+        Dets Section1_15 = new Dets (115 , 21 , "Shadrin-Jerome" , "" , "" , "Male" , "BSIT1", "N/A");
+        Dets Section1_16 = new Dets (116 , 19 , "Karel" , "" , "" , "Female" , "BSIT1", "N/A");
+        Dets Section1_17 = new Dets (117 , 19 , "Anadyl" , "" , "" , "Female" , "BSIT1", "N/A");
+        Dets Section1_18 = new Dets (118 , 21 , "Hamza" , "" , "" , "Male" , "BSIT1" , "N/A");
+        Dets Section1_19 = new Dets (119 , 20 , "Kert-Ice" , "" , "" , "Male" , "BSIT1", "N/A");
+        Dets Section1_20 = new Dets (120 , 21 , "Jeric" , "" , "" , "Male" , "BSIT1", "N/A");
+        Dets Section1_21 = new Dets (121 , 20 , "Angelo" , "" , "" , "Male" , "BSIT1" , "N/A");
+        Dets Section1_22 = new Dets (122 , 19 , "May-Joy" , "" , "" , "Female" , "BSIT1" , "N/A");
+        Dets Section1_23 = new Dets (123 , 19 , "Mary-Jane" , "" , "" , "Female" , "BSIT1", "N/A");
+        Dets Section1_24 = new Dets (124 , 21 , "Jazerll" , "" , "" , "Male" , "BSIT1", "N/A");
+        Dets Section1_25 = new Dets (125 , 20 , "John-Andrej" , "" , "" , "Male" , "BSIT1", "N/A");
         UniqueID uid1 = new UniqueID(101);
         UniqueID uid2 = new UniqueID(102);
         UniqueID uid3 = new UniqueID(103);
@@ -54,12 +51,6 @@ public class Section1 extends AttendanceManagementSystem2 {
         UniqueID uid23 = new UniqueID(123);
         UniqueID uid24 = new UniqueID(124);
         UniqueID uid25 = new UniqueID(125);
-        
-        
-        
-        
-        
-        
         studList.add(Section1_1);
         uniqueid.add(uid1);
 	studList.add(Section1_2);
@@ -110,7 +101,6 @@ public class Section1 extends AttendanceManagementSystem2 {
         uniqueid.add(uid24);
 	studList.add(Section1_25);
         uniqueid.add(uid25);
-        
         choices();
     }
 }  
